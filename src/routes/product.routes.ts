fd20bc32a043998b4eae6f8558046ceca0b00849
magnogouvea <magnogouvea@gmail.com>
2022-11-22 19:06:40 -0300

    Feat: Adiciona req06

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validationsProduct from '../middlewares/validationsProduct';
 
 const router = Router();
 const productController = new ProductController();
 
-router.post('/', productController.create);
+router.post(
+  '/', 
+  validationsProduct.productAmountValidation,
+  validationsProduct.productNameValidation,
+  productController.create,
+);
 router.get('/', productController.getAll);
 
 export default router;
