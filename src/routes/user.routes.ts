4fbbb503d6e560c9b591315e10344717c32ce95f
magnogouvea <magnogouvea@gmail.com>
2022-11-22 21:41:01 -0300

    Feat: Adiciona req07

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import validationsUser from '../middlewares/validationsUser';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/', userController.create);
+router.post(
+  '/',
+  validationsUser.classeValidation,
+  validationsUser.levelValidation,
+  validationsUser.passwordValidation,
+  validationsUser.usernameValidation,
+  userController.create,
+);
 
 export default router;
